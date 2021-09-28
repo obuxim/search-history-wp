c044c379b40cd0fd69d72b7c5d5e35b522eb0896
Zubair Hasan <z@duoneos.com>
2021-09-28 10:59:39 +0600

    Intiailized

---
+++ search-history-wp.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: Search History WP
+Plugin URI: https://github.com/obuxim/search-history-wp
+Description: Saves all the search history into database and creates an endpoint to fetch the history.
+Version: 1.0
+Author: Zubair Hasan
+Author URI: https://zubairhasan.com
+License: MIT
+*/
+
